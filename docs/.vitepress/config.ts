418e3a146a9e6764524c0b2e35b7a4ce14838b80
黄立根 <476421978@qq.com>
2023-09-07 12:23:23 +0800

    配置转ts形式

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "vitepress"
+
+export default defineConfig({
+  // site-level options
+  title: "HLG随笔",
+
+  description: "Just playing around.",
+  themeConfig: {
+    // theme-level options
+  },
+  // ...
+})
